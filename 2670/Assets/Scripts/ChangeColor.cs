6b0b5d4febcd23dcfcd95056ed401105e38d5930
Elijah Celeste <elijah.celeste@gmail.com>
2018-10-03 14:41:24 -0600

    Updated Project Files

---
+++ 2670/Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+	public ColorData SpriteColor;
+
+	private SpriteRenderer sRenderer;
+
+	private void Start()
+	{
+		sRenderer = GetComponent<SpriteRenderer>();
+		sRenderer.color = SpriteColor.Value;
+	}
+}
